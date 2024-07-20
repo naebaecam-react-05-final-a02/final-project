c2dd79ca7c16dc0b0c57b4f3115ce3fd6abd6177
butterbeetle <butterbeetle@naver.com>
2024-07-21 02:19:03 +0900

    Refactor: 챌린지 등록 페이지 컴포넌트로 나누기

---
+++ src/app/(providers)/(root)/challenge/_components/FormTextArea.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { ChangeEvent, useId, useState } from 'react';
+
+type FormTextAreaType = {
+  maxLength: number;
+  label: string;
+  name: string;
+  placeholder: string;
+};
+
+const FormTextArea = ({ maxLength, label, name, placeholder }: FormTextAreaType) => {
+  const id = useId();
+  const [textAreaLength, setTextAreaLength] = useState<number>(0);
+
+  const handleChangeTextArea = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    if (e.target.value.length > maxLength) {
+      e.target.value = e.target.value.slice(0, maxLength);
+    }
+    setTextAreaLength(e.target.value.length);
+  };
+
+  return (
+    <div className="flex flex-col gap-y-2 w-full select-none">
+      <div className="text-xs flex justify-between">
+        <label className="text-xs font-bold" htmlFor={name}>
+          {label}
+        </label>
+        <p className="text-xs text-gray-300">
+          <span>{`${textAreaLength}/${maxLength}`}</span>
+        </p>
+      </div>
+      <textarea
+        maxLength={maxLength}
+        onChange={handleChangeTextArea}
+        className="bg-[#f6f6f6] resize-none w-full p-[10px] placeholder:text-xs outline-none min-h-24
+    focus:outline-none border-b-2 border-b-[#7b7b7b] text-xs"
+        id={id}
+        name={name}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
+
+export default FormTextArea;
--- src/app/(providers)/(root)/challenge/register/_components/ChallengeForm.tsx
+++ src/app/(providers)/(root)/challenge/register/_components/ChallengeForm.tsx
@@ -2,26 +2,16 @@
 
 import { useGetUser } from '@/hooks/auth/useUsers';
 import { createClient } from '@/supabase/client';
-import Image from 'next/image';
-import { ChangeEvent, FormEvent, KeyboardEvent, useRef, useState } from 'react';
-
-const MAX_LENGTH = 300;
+import { FormEvent, useRef } from 'react';
+import FormImageUploader from '../../_components/FormImageUploader';
+import FormInput from '../../_components/FormInput';
+import FormTextArea from '../../_components/FormTextArea';
+import FormCalendar from './FormCalendar';
 
 const ChallengeForm = () => {
   const supabase = createClient();
   const { data: user } = useGetUser();
   const inputRef = useRef<HTMLInputElement>(null);
-  const [imageURL, setImageURL] = useState<string | null>(null);
-  const [textAreaLength, setTextAreaLength] = useState<number>(0);
-
-  const today = new Date(new Date().getTime() + 1000 * 60 * 60 * 9).toISOString().slice(0, 10);
-
-  const handleChangeTextArea = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    if (e.target.value.length > MAX_LENGTH) {
-      e.target.value = e.target.value.slice(0, MAX_LENGTH);
-    }
-    setTextAreaLength(e.target.value.length);
-  };
 
   //TODO 유효성 검사
   //TODO hooks로 로직 따로 빼야함
@@ -75,107 +65,26 @@ const ChallengeForm = () => {
     console.log('RESPONSE___', response);
   };
 
-  const handleFileChange = async (file: File | null) => {
-    if (!file) return;
-    setImageURL(URL.createObjectURL(file));
-  };
-
-  console.log();
-
   return (
     <form onSubmit={(e) => handleSubmit(e)} className="flex flex-col gap-y-4 w-full">
       {/* 사진 */}
-      <div className="w-full select-none">
-        <div className="relative border-4 border-blue-400 border-dashed w-full aspect-video">
-          {imageURL && <Image src={imageURL} alt="ChallengeImg" fill className="object-cover" />}
-          <input
-            ref={inputRef}
-            id="file"
-            type="file"
-            className="hidden"
-            accept=".jpg, .jpeg, .png"
-            onChange={(e) => handleFileChange(e.target.files?.[0] || null)}
-          />
-        </div>
-
-        <label className="w-full bg-blue-300 flex items-center justify-center  cursor-pointer" htmlFor="file">
-          사진을 올려주세요.
-        </label>
-      </div>
+      <FormImageUploader ref={inputRef} />
       {/* 챌린지 이름 */}
-      <div className="flex flex-col gap-y-2 w-full select-none">
-        <label className="text-xs font-bold" htmlFor="title">
-          챌린지 이름
-        </label>
-        <input
-          className="bg-[#f6f6f6] p-[10px] placeholder:text-xs outline-none focus:outline-none border-b-2 border-b-[#7b7b7b] h-8 text-xs"
-          id="title"
-          name="title"
-          type="text"
-          placeholder="누워서 숨쉬기?"
-        />
-      </div>
+      <FormInput label="챌린지 이름" name="title" placeholder="누워서 숨쉬기?" />
 
       {/* 챌린지 내용 */}
-      <div className="flex flex-col gap-y-2 w-full select-none">
-        <div className="text-xs flex justify-between">
-          <label className="text-xs font-bold" htmlFor="content">
-            챌린지 내용
-          </label>
-          <p className="text-xs text-gray-300">
-            <span>{`${textAreaLength}/${MAX_LENGTH}`}</span>
-          </p>
-        </div>
-        <textarea
-          maxLength={MAX_LENGTH}
-          onChange={handleChangeTextArea}
-          className="bg-[#f6f6f6] resize-none w-full p-[10px] placeholder:text-xs outline-none min-h-24
-        focus:outline-none border-b-2 border-b-[#7b7b7b] text-xs"
-          id="content"
-          name="content"
-          placeholder="에어컨 틀고 이불덮고 누워서 티비보기"
-        />
-      </div>
+      <FormTextArea
+        maxLength={300}
+        label="챌린지 내용"
+        name="content"
+        placeholder="에어컨 틀고 이불덮고 누워서 티비보기"
+      />
 
       {/* 챌린지 기간 */}
-      <div className="flex flex-col gap-y-2 w-full select-none">
-        <label className="text-xs font-bold" htmlFor="startDate">
-          챌린지 기간
-        </label>
-        <div className="flex gap-x-1">
-          <input
-            onKeyDown={(e: KeyboardEvent<HTMLInputElement>) => e.preventDefault()}
-            className="flex-1 bg-[#f6f6f6] p-[10px] placeholder:text-xs outline-none focus:outline-none border-b-2 border-b-[#7b7b7b] h-8 text-xs"
-            id="startDate"
-            name="startDate"
-            type="date"
-            min={today}
-            defaultValue={today}
-          />
-          <span>~</span>
-          <input
-            onKeyDown={(e: KeyboardEvent<HTMLInputElement>) => e.preventDefault()}
-            className="flex-1 bg-[#f6f6f6] p-[10px] placeholder:text-xs outline-none focus:outline-none border-b-2 border-b-[#7b7b7b] h-8 text-xs"
-            name="endDate"
-            type="date"
-            min={today}
-          />
-        </div>
-      </div>
+      <FormCalendar />
 
       {/* 인증 방법 */}
-      <div className="flex flex-col gap-y-2 w-full select-none">
-        <label className="text-xs font-bold" htmlFor="desc">
-          인증 방법
-        </label>
-        <input
-          className="bg-[#f6f6f6] p-[10px] placeholder:text-xs outline-none focus:outline-none border-b-2 border-b-[#7b7b7b] h-8 text-xs"
-          id="desc"
-          name="desc"
-          type="text"
-          placeholder="누워서 셀카를 올려주세용"
-        />
-      </div>
+      <FormInput label="인증 방법" name="desc" placeholder="누워서 셀카를 올려주세용" />
 
       <button type="submit" className="select-none w-full rounded-md bg-[#3ecf8e] font-bold py-2">
         입력 안해?
