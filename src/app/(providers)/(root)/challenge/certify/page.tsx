e8ab10ac7bc04ab437dda218d57e786313eb3a0e
butterbeetle <butterbeetle@naver.com>
2024-07-21 21:51:57 +0900

    Refactor: 인증 페이지 ImageUploader 컴포넌트 사용

--- src/app/(providers)/(root)/challenge/certify/page.tsx
+++ src/app/(providers)/(root)/challenge/certify/page.tsx
@@ -1,36 +1,38 @@
 'use client';
 
-import Image from 'next/image';
-import { useId, useState } from 'react';
+import { FormEvent, useRef } from 'react';
+import FormImageUploader from '../_components/FormImageUploader';
 import FormTextArea from '../_components/FormTextArea';
 
 const ChallengeCertifyPage = () => {
-  const [imageURL, setImageURL] = useState<string | null>(null);
-  const id = useId();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const currentTarget = e.currentTarget;
+    const file = inputRef?.current?.files?.[0] || null;
+
+    if (!file) return;
+
+    const formData = new FormData(currentTarget);
+
+    const data = {
+      impression: formData.get('impression'),
+    };
+
+    console.log('DATA___', data);
+  };
 
   return (
-    <main className="grid gap-y-6 w-full">
+    <form onSubmit={handleSubmit} className="grid gap-y-6 w-full">
       {/* 챌린지 이름인가? */}
       <div className="w-full h-14 bg-[#5c5c5c] text-white text-sm flex items-center px-6 rounded-md">
         매일 유산소 챌린지
       </div>
 
       {/* 인증 사진 */}
-      <div className="w-full select-none">
-        <div className="border-2 border-dashed border-blue-400 w-full aspect-video relative">
-          <input
-            id={id}
-            type="file"
-            accept=".jpg, .jpeg, .png"
-            className="hidden"
-            onChange={(e) => setImageURL(URL.createObjectURL(e.target.files?.[0]!))}
-          />
-          {imageURL && <Image className="object-contain" src={imageURL} fill alt="이미지" />}
-        </div>
-        <label className="flex justify-center items-center w-full  bg-blue-200 cursor-pointer" htmlFor={id}>
-          사진 업로드
-        </label>
-      </div>
+      <FormImageUploader ref={inputRef} />
 
       {/* 오늘의 소감 */}
       <FormTextArea
@@ -41,7 +43,7 @@ const ChallengeCertifyPage = () => {
       />
 
       <button className="select-none w-full rounded-md bg-[#3ecf8e] font-bold py-2">제출하기</button>
-    </main>
+    </form>
   );
 };
 
