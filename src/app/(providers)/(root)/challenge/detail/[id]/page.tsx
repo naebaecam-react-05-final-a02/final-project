2f5f900bcd1f749e22c787393333e2467dc6e5a1
sorakim <sora6418@gmail.com>
2024-07-23 07:22:45 +0900

    feat: challenge 상세페이지 컴포넌트 추가

---
+++ src/app/(providers)/(root)/challenge/detail/[id]/page.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import Image from 'next/image';
+import { GetServerSideProps } from 'next';
+const fetchChallenge = async (id: number) => {
+  const res = await fetch(`http://localhost:3000/api/challenge/${id}`);
+  if (!res.ok) {
+    throw new Error('Failed to fetch challenge');
+  }
+  return res.json();
+};
+
+const ChallengeDetailPage = async ({ params }: { params: { id: string } }) => {
+  const id = parseInt(params.id, 10);
+  const challenge = await fetchChallenge(id);
+
+  if (!challenge) {
+    return <div>없따!</div>;
+  }
+  //
+  // const {id} = params
+  // const { data: challenge, isPending, isError, error } = useChallenge(+id);
+  // console.log('@@ challenge', challenge)
+  // if (isPending) return <div>Loading...</div>;
+  // if (error) return <div>Error</div>;
+  // if (!challenge) return <div>No challenges found</div>; // 데이터가 없을 경우 처리
+
+  return (
+    <div className="h-screen">
+      <header className="h-14 bg-amber-600">헤더</header>
+      <main className="bg-amber-300 pb-7">
+        <Image
+          src={challenge.imageURL}
+          alt="썸네일 이미지"
+          width={100}
+          height={100}
+          className="w-full h-[200px] object-cover mb-5"
+        />
+        <section className="flex flex-col gap-9 px-6">
+          <div className="mb-9">
+            <div className="flex flex-row justify-between items-center ">
+              <div>{challenge.title}</div>
+              <div>
+                {challenge.startDate} - {challenge.endDate}
+              </div>
+            </div>
+            <p>{challenge.content}</p>
+          </div>
+          <div>
+            <div className="mb-4">참여 인증 방법</div>
+            <p>
+              {challenge.verify}
+            </p>
+          </div>
+          <div>
+            <h1>후기</h1>
+            <ul>
+              <li>
+                <div>
+                  <span>4.9</span>
+                  <p>
+                    유산소 안하고 집오기~~유산소 안하고 집오기~~유산소 안하고 집오기~~유산소 안하고 집오기~~유산소
+                    안하고 집오기~~
+                  </p>
+                  <span>작성자 - 소다</span>
+                </div>
+              </li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+          <button className="rounded-lg bg-[#3ECF8E] py-2 w-full" type="button">
+            인증하기
+          </button>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default ChallengeDetailPage;
