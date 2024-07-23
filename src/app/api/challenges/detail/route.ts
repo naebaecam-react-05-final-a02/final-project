49ff6347495ea5ad19729efeb8d4545b023a4efc
sorakim <sora6418@gmail.com>
2024-07-23 14:26:20 +0900

    feat: 챌린지 상세 데이터 api route handler로직 추가

---
+++ src/app/api/challenges/detail/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { createClient } from '@/supabase/server';
+
+export async function GET(request:NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get('id');
+  const supabase = createClient();
+
+  if (!id) {
+    return NextResponse.json({ error: 'ID is required' }, { status: 400 });
+  }
+
+  try {
+    const { data, error } = await supabase
+      .from('challenges')
+      .select('*')
+      .eq('id', id)
+      .single();
+
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: 'An unexpected error occurred' }, { status: 500 });
+  }
+}
