b3d13cfd623703c8e030de9cfc7a21fe9c9bb3b2
kwakjaehun <jh5414092@icloud.com>
2024-07-23 17:49:36 +0900

    Feat : layout에 사용될 모바일용 사이즈 컴포넌트 임시로 작성

--- src/layouts/Mobile/Mobile.tsx
+++ src/layouts/Mobile/Mobile.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react';
 
 const Mobile = ({ children }: PropsWithChildren) => {
-  return <div className="max-w-[390px]">{children}</div>;
+  return <div className="max-w-[390px] w-full">{children}</div>;
 };
 
 export default Mobile;
