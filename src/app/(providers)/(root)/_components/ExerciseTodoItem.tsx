9c7df1e6cea1d3183742ba0ac5c12f6bc19b52e1
butterbeetle <butterbeetle@naver.com>
2024-07-25 19:34:24 +0900

    Style: Exercise Todo Style

--- src/app/(providers)/(root)/_components/ExerciseTodoItem.tsx
+++ src/app/(providers)/(root)/_components/ExerciseTodoItem.tsx
@@ -7,22 +7,23 @@ const ExerciseTodoItem = () => {
   const [isChecked, setIsChecked] = useState<boolean>(false);
   return (
     <div className="select-none bg-gradient-to-r from-[#292436] via-[#2e6e56] p-[2px] pt-0">
-      <div className="flex gap-x-4 items-center size-full bg-[#292436]">
-        <div className="size-10 rounded-md bg-[#252b30] p-[2px] relative">
-          <label
-            htmlFor={id}
-            className={`border-2 border-[#36bd73] rounded-md size-full flex items-center justify-center ${
-              isChecked ? 'bg-[#23dd74]' : 'bg-[#292436]'
-            }`}
-          >
-            {isChecked && <p className="text-white text-3xl">{'✔'}</p>}
-            <input type="checkbox" id={id} className="hidden" onChange={() => setIsChecked((prev) => !prev)} />
-          </label>
-        </div>
-        <div className="flex-1">
-          <div className="text-[#c9c6cb]">웜업 스트레칭</div>
-          <div className="text-[#585363]">15분</div>
-        </div>
+      <div className="flex items-center size-full bg-[#292436]">
+        <label className="flex flex-1 gap-x-4 items-center" htmlFor={id}>
+          <div className="size-8 rounded-md bg-[#252b30] p-[2px] relative">
+            <div
+              className={`border-2 border-[#36bd73] rounded-md size-full flex items-center justify-center ${
+                isChecked ? 'bg-[#23dd74]' : 'bg-[#292436]'
+              }`}
+            >
+              {isChecked && <p className="text-white text-lg">{'✔'}</p>}
+              <input type="checkbox" id={id} className="hidden" onChange={() => setIsChecked((prev) => !prev)} />
+            </div>
+          </div>
+          <div className="flex-1">
+            <div className="text-[#c9c6cb]">웜업 스트레칭</div>
+            <div className="text-[#585363]">15분</div>
+          </div>
+        </label>
         <div className="text-[#7d7984]">{'->'}</div>
       </div>
     </div>
--- src/app/(providers)/(root)/_components/ExerciseTodoList.tsx
+++ src/app/(providers)/(root)/_components/ExerciseTodoList.tsx
@@ -4,10 +4,14 @@ import ExerciseTodoItem from './ExerciseTodoItem';
 
 const ExerciseTodoList = () => {
   return (
-    <div className="size-full">
-      <div>헤더헤더</div>
-      <ul className="size-full bg-[#292436] p-4 grid gap-y-5">
-        {Array.from({ length: 3 }, () => '0').map((_, i) => (
+    <div className="size-full bg-[#292436]">
+      <h6 className="text-white flex justify-between px-2">
+        <div className="text-sm">{`< 7/27 >`}</div>
+        <div className="flex-1 flex justify-center">Todo</div>
+        <div>???</div>
+      </h6>
+      <ul className="size-full p-4 grid gap-y-5">
+        {Array.from({ length: 5 }, () => '0').map((_, i) => (
           <li key={i}>
             <ExerciseTodoItem />
           </li>
