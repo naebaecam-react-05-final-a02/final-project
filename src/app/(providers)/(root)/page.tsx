140f0c9826bfc34cc018f8d7fcb296af46b6914a
sorakim <sora6418@gmail.com>
2024-07-26 06:08:18 +0900

    feat: 챌린지 상세 페이지 바로가기 링크 추가

--- src/app/(providers)/(root)/page.tsx
+++ src/app/(providers)/(root)/page.tsx
@@ -5,12 +5,20 @@ const RootPage = () => {
   return (
     <div className="h-screen">
       <p>메인 페이지</p>
-      <Link
-        className="bg-[#A6A6A6] rounded-md pt-2 pb-2 pl-6 pr-6 transition-all duration-300 ease-in-out hover:translate-y-1 active:translate-y-2 hover:shadow-md border-b-4 border-[#858585]"
-        href={'/auth_test'}
-      >
-        Auth 관련
-      </Link>
+      <div className="flex flex-row gap-3">
+        <Link
+          className="bg-[#A6A6A6] rounded-md pt-2 pb-2 pl-6 pr-6 transition-all duration-300 ease-in-out hover:translate-y-1 active:translate-y-2 hover:shadow-md border-b-4 border-[#858585]"
+          href={'/auth_test'}
+        >
+          Auth 관련
+        </Link>
+        <Link
+          className="bg-[#A6A6A6] rounded-md pt-2 pb-2 pl-6 pr-6 transition-all duration-300 ease-in-out hover:translate-y-1 active:translate-y-2 hover:shadow-md border-b-4 border-[#858585]"
+          href={'/challenge/detail/15'}
+        >
+          챌린지 상세 관련
+        </Link>
+      </div>
       <DashBoard />
     </div>
   );
