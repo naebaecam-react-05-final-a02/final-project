1b781fd531acd10a44d27d23157bc49d53a7a272
sorakim <sora6418@gmail.com>
2024-07-26 05:36:37 +0900

    feat: 챌린지 항목 조회 쿼리, Hook 추가

--- src/hooks/challenge/queries.ts
+++ src/hooks/challenge/queries.ts
@@ -1,6 +1,23 @@
 import api from '@/service/service';
 import { Tables } from '@/types/supabase';
 
+export const challengesQueryKeys = {
+  all: ['challenge'] as const,
+};
+export const queryOptions = {
+  getChallengeDetail: (id: number) => ({
+    queryKey: challengesQueryKeys.all,
+    queryFn: async () => {
+      const data = await api.challenge.getChallengeDetail(id);
+
+      if (!data) {
+        throw new Error('data not found');
+      }
+      return data;
+    },
+  }),
+};
+
 export const mutationOptions = {
   register: {
     mutationFn: (challengeData: Omit<Tables<'challenges'>, 'id'>) => api.challenge.register(challengeData),
