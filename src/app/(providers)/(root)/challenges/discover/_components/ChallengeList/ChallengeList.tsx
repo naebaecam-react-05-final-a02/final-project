36e2bff1b1d5d1a4c98bf0500dbfaedf3c328523
kwakjaehun <jh5414092@icloud.com>
2024-07-26 15:22:45 +0900

    Fix: 버그 수정

--- src/app/(providers)/(root)/challenges/discover/_components/ChallengeList/ChallengeList.tsx
+++ src/app/(providers)/(root)/challenges/discover/_components/ChallengeList/ChallengeList.tsx
@@ -10,7 +10,7 @@ const ChallengeList = ({ data }: ChallengeListProps) => {
     <ul className="grid grid-cols-2 gap-2">
       {data.map((challenge: any) => (
         <li key={challenge.id}>
-          <Link href={`/challenge/detail/${challenge.id}`}>
+          <Link href={`/challenges/${challenge.id}/detail`}>
             <ChallengeCard challenge={challenge} />
           </Link>
         </li>
