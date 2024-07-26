c22b3ee34defa1d779e557a90e9cf9b67a46ccfe
Jongha <jongha1230@gmail.com>
2024-07-26 19:54:24 +0900

    fix: 충돌 해결

--- src/app/(providers)/(root)/challenges/discover/_components/PopularChallenges/PopularChallenges.tsx
+++ src/app/(providers)/(root)/challenges/discover/_components/PopularChallenges/PopularChallenges.tsx
@@ -34,22 +34,6 @@ const PopularChallenges = async () => {
     console.error('Error fetching popular challenges:', error);
     return <div>인기 있는 챌린지를 불러오는 중 오류가 발생했습니다.</div>;
   }
-    return (
-      <div className="flex flex-col gap-4">
-        <div className="flex justify-between items-center">
-          <h2 className="font-bold text-3xl">인기있는 챌린지</h2>
-          <Link href="/challenges/discover/popular">
-            <div>전체보기</div>
-          </Link>
-        </div>
-        <Categories />
-        <ChallengeList data={data} />
-      </div>
-    );
-  } catch (error) {
-    console.error('Error fetching popular challenges:', error);
-    return <div>인기 있는 챌린지를 불러오는 중 오류가 발생했습니다.</div>;
-  }
 };
 
 export default PopularChallenges;
