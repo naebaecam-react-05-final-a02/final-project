31205580dbde07663b9440edb3c3cdc348f61aff
butterbeetle <butterbeetle@naver.com>
2024-07-26 20:21:57 +0900

    Update: 인증 목록 카드에 날짜,시간 추가/ 닉네임 변경

--- src/app/(providers)/(root)/challenges/[id]/verification/list/_components/VerificationCard.tsx
+++ src/app/(providers)/(root)/challenges/[id]/verification/list/_components/VerificationCard.tsx
@@ -1,14 +1,22 @@
 import { verificationsType } from '@/types/challenge';
+import { format } from 'date-fns';
 import Image from 'next/image';
 
 const VerificationCard = ({ verification }: { verification: verificationsType }) => {
-  const nickname = verification.users.nickname ? verification.users.nickname : '헬린이';
+  const userNumber = verification.userId.split('-')[1];
+  const nickname = verification.users.nickname ? verification.users.nickname : `헬린이_${userNumber}`;
 
   return (
-    <div className=" p-4 flex flex-col gap-y-4 border border-gray-400 rounded-lg">
-      <div className="flex gap-x-2 items-center">
-        <div className="rounded-full bg-gray-200 size-8">{/* 프로필 사진*/}</div>
-        <div className="text-sm font-bold">{nickname}</div>
+    <div className=" p-4 flex flex-col gap-y-4 border border-gray-400 rounded-lg shadow-lg">
+      <div className="flex justify-between items-center w-full">
+        <div className="flex gap-x-4 items-center">
+          <div className="rounded-full bg-gray-200 size-8">{/* 프로필 사진*/}</div>
+          <div className="text-sm font-bold">{nickname}</div>
+        </div>
+        <div className="text-right">
+          <div className="text-xs text-gray-600">{format(verification.date!, 'yyyy-MM-dd')}</div>
+          <div className="text-xs text-gray-500">{format(verification.date!, 'h:mm:ss bb')}</div>
+        </div>
       </div>
       <div className="size-full bg-white">
         <div className="w-full aspect-video  text-white border border-gray-500 flex items-center justify-center relative">
