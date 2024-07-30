45f7d958f85a5c9f1123f12b01d957cf9ffc285d
Jongha <jongha1230@gmail.com>
2024-07-30 22:34:24 +0900

    fix: AddButton svg 수정

--- src/components/Header/AddButton/AddButton.tsx
+++ src/components/Header/AddButton/AddButton.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import ChevronLeft from '@/icons/ChevronLeft';
+import Puls from '@/icons/Puls';
 import { ComponentProps } from 'react';
 
 interface BackButtonProps extends ComponentProps<'button'> {
@@ -23,7 +23,7 @@ const AddButton = ({ onAddClick, className = '', ...props }: BackButtonProps) =>
       className={`rounded-xl border-2 border-black/10 shadow-[2px_2px_4px_0px_rgba(0,0,0,0.25),-2px_-2px_4px_0px_rgba(255,255,255,0.10)] backdrop-blur-[5px] p-2 ${className}`}
       {...props}
     >
-      <ChevronLeft />
+      <Puls />
     </button>
   );
 };
