fa67674d4fe3cc9edd1d81d92fdf38928e40cba1
kwakjaehun <jh5414092@icloud.com>
2024-07-30 22:59:04 +0900

    Feat : exercise record page tab 기능 구현

--- src/app/(providers)/(root)/exercises/_components/FormItem/FormItem.tsx
+++ src/app/(providers)/(root)/exercises/_components/FormItem/FormItem.tsx
@@ -1,5 +1,49 @@
-const FormItem = () => {
-  return <div>FormItem</div>;
+import { useCardioInputStore, useWeightInputStore } from '@/stores/useExerciseStore';
+
+interface FormItemProps {
+  index: number;
+  firstProp: number;
+  secondProp: number;
+  type: 'cardio' | 'weight';
+  onChange: (e: React.ChangeEvent<HTMLInputElement>, index: number) => void;
+}
+
+const FormItem = ({ onChange, type, index, firstProp, secondProp }: FormItemProps) => {
+  const deleteCardio = useCardioInputStore((state) => state.deleteInput);
+  const deleteWeight = useWeightInputStore((state) => state.deleteInput);
+  const deleteInput = () => {
+    if (type === 'cardio') {
+      deleteCardio(index);
+    } else {
+      deleteWeight(index);
+    }
+  };
+  return (
+    <div className="grid grid-cols-4 justify-items-center">
+      <div className="flex justify-center items-center w-12 h-10 border-2 border-black">{index + 1}</div>
+      <input
+        onChange={(e) => onChange(e, index)}
+        className="flex justify-center items-center w-12 h-10 border-2 border-black exerciseInput"
+        name={type === 'cardio' ? 'hours' : 'weight'}
+        type="number"
+        value={firstProp}
+      />
+      <input
+        onChange={(e) => onChange(e, index)}
+        className="flex justify-center items-center w-12 h-10 border-2 border-black exerciseInput"
+        name={type === 'cardio' ? 'minutes' : 'reps'}
+        type="number"
+        value={secondProp}
+      />
+      <button
+        type="button"
+        className="flex justify-center items-center w-12 h-10 border-2 border-black"
+        onClick={deleteInput}
+      >
+        삭제
+      </button>
+    </div>
+  );
 };
 
 export default FormItem;
