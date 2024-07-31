758ea05a1c10592aed71eaac23c77dc4bc11976b
butterbeetle <butterbeetle@naver.com>
2024-07-31 20:10:11 +0900

    Refactor: 대시보드 식단 header 변경

--- src/app/(providers)/(root)/_components/DietsLog.tsx
+++ src/app/(providers)/(root)/_components/DietsLog.tsx
@@ -6,10 +6,9 @@ import { getDiets } from '@/hooks/dashboard/useDashBoard';
 import { createClient } from '@/supabase/client';
 import { getDietsCalories, getFoods } from '@/utils/calculateDiet';
 import { useQuery } from '@tanstack/react-query';
-import { addDays, format, subDays } from 'date-fns';
+import { format } from 'date-fns';
 import { useState } from 'react';
-import { IoMdArrowDropleft, IoMdArrowDropright } from 'react-icons/io';
-import { IoCreateOutline } from 'react-icons/io5';
+import DashBoardHeader from './DashBoardHeader';
 
 const DietsLog = () => {
   const supabase = createClient();
@@ -25,35 +24,9 @@ const DietsLog = () => {
   const calories = getDietsCalories(diets?.data);
   const foods = getFoods(diets?.data);
 
-  const handleNextDay = () => {
-    setDate((prev) => addDays(prev, 1));
-  };
-
-  const handlePrevDay = () => {
-    setDate((prev) => subDays(prev, 1));
-  };
-
   return (
     <Card className="bg-white size-full relative px-[-20px] text-sm flex flex-col items-center select-none">
-      <div className="relative flex text-white items-center justify-between w-full">
-        <div className="text-xs flex items-center gap-x-1">
-          <div className="text-base cursor-pointer" onClick={handlePrevDay}>
-            <IoMdArrowDropleft />
-          </div>
-
-          <div className="cursor-pointer w-6 text-center">{format(date, 'M/d')}</div>
-
-          <div className="text-base cursor-pointer" onClick={handleNextDay}>
-            <IoMdArrowDropright />
-          </div>
-        </div>
-
-        <div className="absolute opacity-50 text-base left-1/2 transform -translate-x-1/2">식단</div>
-
-        <div className="cursor-pointer text-xl">
-          <IoCreateOutline />
-        </div>
-      </div>
+      <DashBoardHeader date={date} setState={setDate} />
 
       <div className="w-full">
         <div className="flex justify-between items-center h-[44px] relative">
