e0cf48bc374b03b594c7d09c2dbda07c88c1b058
sorakim <sora6418@gmail.com>
2024-08-02 05:06:04 +0900

    feat: 운동기록 - 북마크 쿼리스 추가

---
+++ src/hooks/exercises/queries.ts
@@ -0,0 +1,26 @@
+import api from '@/service/service';
+import { RecordData } from '@/types/exercises';
+
+export const ExercisesQueryKeys = {
+  all: ['exercises'] as const,
+  bookmark: () => [...ExercisesQueryKeys.all, 'bookmark'] as const,
+};
+export const queryOptions = {
+  getExercisesBookmarks: () => ({
+    queryKey: ExercisesQueryKeys.bookmark(),
+    queryFn: async () => {
+      const data = await api.exercise.getBookmarks();
+
+      if (!data) {
+        throw new Error('data not found');
+      }
+      return data;
+    },
+  }),
+};
+
+export const mutationOptions = {
+  register: {
+    mutationFn: (exerciseData: RecordData) => api.exercise.register(exerciseData),
+  },
+};
