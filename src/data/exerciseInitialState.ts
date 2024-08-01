2fabe175c2e798b2909f3b732e6fd7d89569438c
sorakim <sora6418@gmail.com>
2024-08-01 15:39:23 +0900

    feat: exerciseInitial data 추가

---
+++ src/data/exerciseInitialState.ts
@@ -0,0 +1,9 @@
+import { ExerciseRecord } from '@/types/exercises';
+import React from 'react';
+
+export const exerciseInitialState: ExerciseRecord = {
+  date: '',
+  name: '',
+  record: [],
+  exerciseType: 'weight',
+};
