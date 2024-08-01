0699a993598b1c33a46232a80d50b3a4a64ac605
sorakim <sora6418@gmail.com>
2024-08-01 15:42:30 +0900

    feat: 운동 기록 페이지 컴포넌트 추가

--- src/app/(providers)/(root)/exercises/record/page.tsx
+++ src/app/(providers)/(root)/exercises/record/page.tsx
@@ -1,6 +1,10 @@
 'use client';
-import React, { useState } from 'react';
-import ExerciseCreatePage from '../create/page';
+import Input from '@/components/Input';
+import { exerciseInitialState } from '@/data/exerciseInitialState';
+import { CardioInput, ExerciseRecord, ExerciseType, WeightInput } from '@/types/exercises';
+import React, { useEffect, useState } from 'react';
+import ExerciseRecordForm from './_components/exerciseRecordForm/ExerciseRecordForm';
+import Star from '@/icons/Star';
 
 const ExerciseRecordPage = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -15,11 +19,13 @@ const ExerciseRecordPage = () => {
     '덤벨 컬',
     '푸시업',
   ]);
-  const [record, setRecord] = useState([]);
+  const [record, setRecord] = useState<ExerciseRecord>(exerciseInitialState);
+
+  const [isBookMark, setIsBookMark] = useState(false);
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
-    setCustomWorkout(event.target.value); // 새로운 운동 이름으로 설정
+    setCustomWorkout(event.target.value);
   };
 
   const filteredWorkouts = favoriteWorkouts.filter((workout) =>
@@ -33,33 +39,40 @@ const ExerciseRecordPage = () => {
   };
 
   const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDate(event.target.value);
+    setRecord((prev) => ({
+      ...prev,
+      date: event.target.value,
+    }));
   };
 
   const handleMemoChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setMemo(event.target.value);
+    setRecord((prev) => ({
+      ...prev,
+      name: event.target.value,
+    }));
   };
 
   const handleSubmit = async () => {
     const workoutToSave = selectedWorkout || customWorkout;
     console.log('선택한 운동 이름:', workoutToSave);
-    console.log('선택한 날짜:', date);
-    console.log('메모:', memo);
-    console.log('기록:', record);
+    console.log('선택한 날짜:', record.date);
+    console.log('메모:', record.name);
+    console.log('기록:', record.record);
 
     // 데이터가 없는 경우 빠르게 반환
-    if (!workoutToSave || !date || record.length === 0) {
-      console.error('필수 입력 사항 누락:', { workoutToSave, date, recordLength: record.length });
-      alert('운동 이름, 날짜, 세트는 필수 입력 사항입니다.');
+    if (!record.date || record.record.length === 0) {
+      console.error('필수 입력 사항 누락:', { workoutToSave, date: record.date, recordLength: record.record.length });
+      alert('운동 이름, 날짜, 세트는 필수 입력 사항~~');
       return;
     }
 
     const requestBody = {
-      date,
-      exerciseType: workoutToSave,
-      name: workoutToSave,
-      record,
-      memo,
+      date: record.date,
+      exerciseType: record.exerciseType,
+      // name: workoutToSave,
+      record: record.record,
+      name: record.name,
+      isBookMark: isBookMark,
     };
 
     try {
@@ -73,7 +86,7 @@ const ExerciseRecordPage = () => {
 
       if (!response.ok) {
         const errorData = await response.json();
-        throw new Error(errorData.message || '네트워크 응답이 올바르지 않습니다.');
+        throw new Error(errorData.message || 'errorrrr');
       }
 
       const result = await response.json();
@@ -85,22 +98,48 @@ const ExerciseRecordPage = () => {
       setCustomWorkout('');
       setDate('');
       setMemo('');
-      setRecord([]);
+      setRecord(exerciseInitialState);
+      setIsBookMark(false);
     } catch (error) {
       console.error('데이터 전송 중 오류 발생:', error);
-      alert('데이터 전송에 실패했습니다. 다시 시도해 주세요.');
     }
   };
+  const handleChange = (data: CardioInput[] | WeightInput[], type: ExerciseType) => {
+    setRecord((prev) => ({
+      ...prev,
+      record: data,
+      // record: [{ weight: 30, reps: 10 }],
+      exerciseType: type,
+    }));
+    console.log('@@Record', record.record);
+  };
+
+  const handleToggleBookmark = () => {
+    setIsBookMark((prev) => !prev);
+  };
+
+  useEffect(() => {
+    console.log('@@RECORD.', record.record);
+  }, [record.record]);
 
   return (
     <div className="min-h-screen bg-slate-500 flex flex-col gap-5 p-5">
       <h3 className="text-white">운동 이름</h3>
-      <input
-        type="text"
+      <Input
+        label="운동 이름"
         placeholder="운동 이름을 입력해 주세요."
         value={searchTerm}
         onChange={handleSearchChange}
-        className="p-2 rounded"
+        icon={
+          <Star
+            style={{
+              fill: isBookMark ? '#12F287' : 'none',
+            }}
+            width={24}
+            height={24}
+            onClick={handleToggleBookmark}
+          />
+        }
       />
       {searchTerm && filteredWorkouts.length > 0 && (
         <ul className="bg-white rounded p-2">
@@ -119,14 +158,14 @@ const ExerciseRecordPage = () => {
         <p className="text-white">선택된 운동: {selectedWorkout || customWorkout}</p>
       )}
       <h3 className="text-white">날짜 선택</h3>
-      <input type="date" value={date} onChange={handleDateChange} className="p-2 rounded" />
+      <input type="date" value={record.date} onChange={handleDateChange} className="p-2 rounded" />
       <textarea
         placeholder="주의사항, 다짐 등을 작성해 주세요"
-        value={memo}
+        value={record.name}
         onChange={handleMemoChange}
         className="p-2 rounded"
       />
-      <ExerciseCreatePage onSubmit={setRecord} />
+      <ExerciseRecordForm onChange={handleChange} />
       <button type="submit" onClick={handleSubmit} className="bg-blue-500 text-white p-2 rounded mt-2">
         등록하기
       </button>
