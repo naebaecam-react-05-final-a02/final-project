08fb54b5447aaf94c6871dfda259d4ae19bcd4fd
butterbeetle <butterbeetle@naver.com>
2024-08-04 17:28:17 +0900

    Update: 인증 시 "오늘" 인증한 사람 가져오도록 수정 - 기존에는 전체 기간

--- src/app/(providers)/(root)/challenges/[id]/verification/register/page.tsx
+++ src/app/(providers)/(root)/challenges/[id]/verification/register/page.tsx
@@ -2,6 +2,7 @@ import Button from '@/components/Button';
 import Mobile from '@/layouts/Mobile';
 import { createClient } from '@/supabase/server';
 import { Tables } from '@/types/supabase';
+import { getStartOfDayISO } from '@/utils/dateFormatter';
 import Link from 'next/link';
 import VerificationRegister from './_components/registerVerification/VerificationRegister';
 
@@ -36,6 +37,8 @@ const ChallengeVerificationRegisterPage = async ({ params }: { params: { id: str
     const { data } = await supabase
       .from('users')
       .select('id,profileURL')
+      .gte('date', getStartOfDayISO())
+      .lte('date', getStartOfDayISO())
       .in('id', userIds)
       .returns<{ id: string; profileURL: string | null }[]>();
 
