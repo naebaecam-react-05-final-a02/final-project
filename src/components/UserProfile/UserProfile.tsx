8bbdec2bcf89a14d4ac0b5123114651b4545fd71
sorakim <sora6418@gmail.com>
2024-08-04 15:47:46 +0900

    update: 유저 프로필 이미지 넘치는 부분 스타일 처리

--- src/components/UserProfile/UserProfile.tsx
+++ src/components/UserProfile/UserProfile.tsx
@@ -3,7 +3,11 @@
 import { useGetUser } from '@/hooks/auth/useUsers';
 import Image from 'next/image';
 
-const UserProfile = () => {
+interface UserProfileProps {
+  className?: string;
+}
+
+const UserProfile = ({ className }: UserProfileProps) => {
   const { data: user, error, isLoading } = useGetUser();
 
   if (isLoading) return <div>Loading...</div>;
@@ -11,7 +15,7 @@ const UserProfile = () => {
 
   return (
     <div className="flex items-center gap-2 h-14 ">
-      <div className="relative w-9 h-9 border-white border rounded-full">
+      <div className="relative w-9 h-9 border-white border rounded-full overflow-hidden">
         <Image src={user?.profileURL ?? '/default-profile.png'} alt={'username'} fill style={{ objectFit: 'cover' }} />
       </div>
       <div className="text-sm">{user?.nickname}</div>
