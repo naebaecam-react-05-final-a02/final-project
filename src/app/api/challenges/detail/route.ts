d4abf95d11f787028062f211ad0abcf5aa7c36d4
sorakim <sora6418@gmail.com>
2024-08-05 11:29:17 +0900

    update: detail route 유저 정보 가져오는 코드 변경

--- src/app/api/challenges/detail/route.ts
+++ src/app/api/challenges/detail/route.ts
@@ -14,7 +14,7 @@ export async function GET(request: NextRequest) {
   try {
     const { data, error } = await supabase
       .from('challenges')
-      .select('*,participants:challengeParticipants(userId)')
+      .select('*,participants:challengeParticipants(userId),user:users(*)')
       .eq('id', id)
       .single();
 
