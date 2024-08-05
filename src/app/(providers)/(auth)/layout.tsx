6cc6e57fd54c4e582ed63640db735121b7b74053
Jongha <jongha1230@gmail.com>
2024-08-05 12:22:37 +0900

    fix: 레이아웃 배경 수정

--- src/app/(providers)/(auth)/layout.tsx
+++ src/app/(providers)/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="w-full h-full bg-[#1A1625]">
-      <div className="flex items-center justify-center  bg-[#1A1625]  w-full h-screen bg-[url('/background.png')] bg-cover bg-center text-white ">
+      <div className="flex items-center justify-center  bg-[#1A1625]  w-full min-h-screen bg-[url('/background.png')] bg-cover bg-center text-white ">
         {children}
       </div>
     </div>
--- src/app/(providers)/(auth)/sign-up/_components/SignUpForm/SignUpForm.tsx
+++ src/app/(providers)/(auth)/sign-up/_components/SignUpForm/SignUpForm.tsx
@@ -18,8 +18,8 @@ import PhysicalInfoForm from '../PhysicalInfoForm';
 import WelcomePreview from '../WelcomePreview';
 
 const SignUpForm = () => {
-  // const [currentStep, setCurrentStep] = useState('essentialInfo');
-  const [currentStep, setCurrentStep] = useState('success1');
+  const [currentStep, setCurrentStep] = useState('essentialInfo');
+  // const [currentStep, setCurrentStep] = useState('success1');
   const [formState, setFormState] = useState<FormState>(initialFormState);
   const route = useRouter();
 
@@ -70,7 +70,7 @@ const SignUpForm = () => {
   };
 
   return (
-    <div className="flex flex-col h-screen justify-between items-end w-full pb-20">
+    <div className="flex flex-col min-h-screen justify-between items-end w-full pb-20">
       <div className="w-full">
         {currentStep === 'essentialInfo' ? <Header title="회원가입" /> : <Header showLogo showBackButton={false} />}
         {currentStep === 'essentialInfo' && (
