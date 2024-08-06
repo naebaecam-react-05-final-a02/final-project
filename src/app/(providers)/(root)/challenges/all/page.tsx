9bb1c297e33fc3c9c0255c5281af8d38cc52874e
kwakjaehun <jh5414092@icloud.com>
2024-08-06 11:22:17 +0900

    Fix : 버그 수정

--- src/app/(providers)/(root)/challenges/all/page.tsx
+++ src/app/(providers)/(root)/challenges/all/page.tsx
@@ -1,4 +1,5 @@
 import Mobile from '@/layouts/Mobile';
+import Link from 'next/link';
 import ChallengeList from '../_components/ChallengeList';
 import OrderTab from './_components/OrderTab/OrderTab';
 
@@ -6,8 +7,10 @@ const AllChallengesPage = () => {
   return (
     <Mobile>
       <section className="px-4 flex flex-col gap-2">
+        <div className="w-full flex justify-end text-primary-100">
+          <Link href={'/challenges/register'}>글 작성하기</Link>
+        </div>
         <OrderTab />
-
         <ChallengeList />
       </section>
     </Mobile>
