ced98d1aa42e0dfe971fa9205f46f2b2375f5074
kwakjaehun <jh5414092@icloud.com>
2024-08-07 13:45:05 +0900

    Fix : 목업용 모바일 레이아웃 수정

--- src/app/(providers)/(root)/challenges/_components/MyChallenges/MyChallenges.tsx
+++ src/app/(providers)/(root)/challenges/_components/MyChallenges/MyChallenges.tsx
@@ -15,7 +15,7 @@ const MyChallenges = ({ data }: MyChallengesProps) => {
     <section className="flex flex-col gap-4 px-4">
       <div className="w-full flex justify-between items-center">
         <ArticleTitle icon="🤛" title="내가 참여중인 챌린지" />
-        <p className="text-white/50 text-sm">총 10개</p>
+        <p className="text-white/50 text-sm">총 {challenges?.length}개</p>
       </div>
       <ul>
         {_.isEmpty(challenges) ? (
--- src/layouts/Mobile/_components/FrameFooter/FrameFooter.tsx
+++ src/layouts/Mobile/_components/FrameFooter/FrameFooter.tsx
@@ -1,9 +1,33 @@
+import HttpsIcon from '@/assets/mockup/https.svg';
+import RefreshIcon from '@/assets/mockup/replay.svg';
+import LanguageIcon from '@/assets/mockup/spellcheck.svg';
+
+import PrevIcon from '@/assets/mockup/arrow_back_ios.svg';
+import NextIcon from '@/assets/mockup/arrow_forward_ios.svg';
+import ShareIcon from '@/assets/mockup/cloud_upload.svg';
+import BookmarkIcon from '@/assets/mockup/turned_in_not.svg';
+import PagesIcon from '@/assets/mockup/width_normal.svg';
+
 const FrameFooter = () => {
   return (
-    <div className="px-5 py-2 bg-black box-border">
-      <div className="rounded-xl w-full h-11 bg-[rgba(238, 238, 239, 0.19)]"></div>
-      <div className="h-11 w-full absolute bottom-0 flex justify-center">
-        <div className="w-40 h-1 rounded-full absolute bottom-2 bg-white"></div>
+    <div className="px-5 py-2 bg-[#2d2d2d] box-border w-full h-[130px] relative flex flex-col items-center">
+      <div className="rounded-xl w-full h-11 bg-white/50 px-2 flex items-center justify-between gap-1">
+        <LanguageIcon width={24} height={24} />
+        <div className="flex gap-2">
+          <HttpsIcon />
+          <p className="text-white text-lg">https://www.oosie.com</p>
+        </div>
+        <RefreshIcon width={24} height={24} />
+      </div>
+      <div className="w-full h-11 flex items-center justify-between">
+        <PrevIcon width={24} height={24} />
+        <NextIcon width={24} height={24} />
+        <ShareIcon width={24} height={24} />
+        <BookmarkIcon width={24} height={24} />
+        <PagesIcon width={24} height={24} />
+      </div>
+      <div className="w-full h-[26px] flex items-end justify-center">
+        <div className="w-40 h-1 rounded-full bg-white"></div>
       </div>
     </div>
   );
