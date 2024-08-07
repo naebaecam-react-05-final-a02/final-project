303c757dd74a82286c9f3f1f7f44d1e9878c161e
kwakjaehun <jh5414092@icloud.com>
2024-08-07 13:56:01 +0900

    Fix : 설명 부분 추가

--- src/layouts/Mobile/_components/MockUp/MockUp.tsx
+++ src/layouts/Mobile/_components/MockUp/MockUp.tsx
@@ -5,7 +5,10 @@ import FrameHeader from '../FrameHeader';
 
 const MockUp = ({ children }: PropsWithChildren) => {
   return (
-    <main className="w-full h-screen flex justify-center items-center">
+    <main className="w-full h-screen flex justify-center items-center gap-4">
+      <article className="w-[400px]">
+        <h1 className="text-2xl font-bold">MOCK UP</h1>
+      </article>
       <article className="border-black border-[10px] relative rounded-[40px] overflow-hidden">
         <div className="w-[390px] h-[844px] overflow-hidden relative flex flex-col">
           <FrameHeader />
