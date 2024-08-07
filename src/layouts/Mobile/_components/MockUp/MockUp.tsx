1e84d19d26f37c53cf2778abaf096529370fbd56
kwakjaehun <jh5414092@icloud.com>
2024-08-07 19:22:44 +0900

    Feat : desktop용 mobile mockup 구현

--- src/layouts/Mobile/_components/MockUp/MockUp.tsx
+++ src/layouts/Mobile/_components/MockUp/MockUp.tsx
@@ -1,22 +1,29 @@
+import Frame from '@/assets/mockup/frame.svg';
 import { PropsWithChildren } from 'react';
-import BackBoard from '../../BackBoard/BackBoard';
+import BackBoard from '../../BackBoard';
+import GlobalBackBoard from '../../GlobalBackBoard';
 import FrameFooter from '../FrameFooter';
 import FrameHeader from '../FrameHeader';
+import Introduction from '../Introduction';
 
 const MockUp = ({ children }: PropsWithChildren) => {
   return (
-    <main className="w-full h-screen flex justify-center items-center gap-4">
-      <article className="w-[400px]">
-        <h1 className="text-2xl font-bold">MOCK UP</h1>
-      </article>
-      <article className="border-black border-[10px] relative rounded-[40px] overflow-hidden">
-        <div className="w-[390px] h-[844px] overflow-hidden relative flex flex-col">
-          <FrameHeader />
-          {children}
-          <FrameFooter />
+    <main className="w-full h-screen flex justify-evenly items-center">
+      <Introduction />
+      <div className="relative flex justify-center items-center">
+        <article className="border-black relative rounded-[60px] overflow-hidden">
+          <div className="w-[390px] h-[844px] overflow-hidden relative flex flex-col">
+            <FrameHeader />
+            {children}
+            <FrameFooter />
+          </div>
+          <BackBoard />
+        </article>
+        <div className="absolute top -z-40">
+          <Frame width={410} height={864} />
         </div>
-        <BackBoard />
-      </article>
+      </div>
+      <GlobalBackBoard />
     </main>
   );
 };
