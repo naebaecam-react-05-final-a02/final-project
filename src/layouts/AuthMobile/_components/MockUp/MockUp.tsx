ce1e15aae61d1d29a3becb09bd7491c933efff36
kwakjaehun <jh5414092@icloud.com>
2024-08-07 20:29:49 +0900

    Fix : Auth 레이아웃 수정:

--- src/layouts/AuthMobile/_components/MockUp/MockUp.tsx
+++ src/layouts/AuthMobile/_components/MockUp/MockUp.tsx
@@ -1,19 +1,29 @@
+import Frame from '@/assets/mockup/frame.svg';
+import Introduction from '@/layouts/Mobile/_components/Introduction';
+import GlobalBackBoard from '@/layouts/Mobile/GlobalBackBoard';
 import { PropsWithChildren } from 'react';
 import BackBoard from '../../BackBoard/BackBoard';
 import FrameHeader from '../FrameHeader';
 
 const MockUp = ({ children }: PropsWithChildren) => {
   return (
-    <main className="w-full h-screen flex justify-center items-center">
-      <article className="border-black border-[10px] relative rounded-[40px] overflow-hidden">
-        <div className="w-[390px] h-[844px] overflow-hidden relative flex flex-col justify-between">
-          <div>
-            <FrameHeader />
-            {children}
+    <main className="w-full h-screen flex justify-evenly items-center">
+      <Introduction />
+      <div className="relative flex justify-center items-center">
+        <article className="border-black relative rounded-[60px] overflow-hidden">
+          <div className="w-[390px] h-[844px] overflow-hidden relative flex flex-col justify-between">
+            <div>
+              <FrameHeader />
+              {children}
+            </div>
           </div>
+          <BackBoard />
+        </article>
+        <div className="absolute top -z-40">
+          <Frame width={410} height={864} />
         </div>
-        <BackBoard />
-      </article>
+      </div>
+      <GlobalBackBoard />
     </main>
   );
 };
