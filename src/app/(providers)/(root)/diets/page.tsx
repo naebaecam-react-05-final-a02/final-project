5f056add0c65484aa7b434aed41386f5153255e6
2hanbyeol1 <2hanbyeol1@naver.com>
2024-08-08 12:08:37 +0900

    update: 달력으로 날짜 선택

--- src/app/(providers)/(root)/diets/page.tsx
+++ src/app/(providers)/(root)/diets/page.tsx
@@ -1,17 +1,22 @@
 'use client';
 import Week from '@/components/Calendar/Week';
 import Header from '@/components/Header';
+import InputCalendar from '@/components/Input/InputDate/InputCalendar';
 import Mobile from '@/layouts/Mobile';
 import useDateStore from '@/stores/date.store';
 import useDietStore from '@/stores/diet.store';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import DietList from './_components/DietList';
+import DownIcon from '/public/icons/chevron-down.svg';
 import PlusIcon from '/public/icons/plus.svg';
 
 const DietPage = () => {
   const router = useRouter();
+  const [isCalendarOpen, setIsCalendarOpen] = useState(false);
 
   const selectedDate = useDateStore((store) => store.date);
+  const setSelectedDate = useDateStore((store) => store.setDate);
   const setDiet = useDietStore((state) => state.setDiet);
 
   const handleAddButtonClick = () => {
@@ -19,46 +24,42 @@ const DietPage = () => {
     router.push('/diets/write');
   };
 
+  const handleDownButtonClick = () => {
+    setIsCalendarOpen(!isCalendarOpen);
+  };
+
+  const handleDateSelected = (date: Date) => {
+    setSelectedDate(date);
+    setIsCalendarOpen(false);
+  };
+
   return (
     <Mobile
       headerLayout={
         <Header
-          title={`${selectedDate.getFullYear()}년${selectedDate.getMonth() + 1}월`}
+          title={`${selectedDate.getFullYear()}년 ${selectedDate.getMonth() + 1}월`}
           icon={<PlusIcon className="w-6 h-6" onClick={handleAddButtonClick} />}
-          // titleIcon={<DownIcon />}
+          titleIcon={
+            <DownIcon
+              className={`transition-transform ${isCalendarOpen && 'rotate-180'}`}
+              onClick={handleDownButtonClick}
+            />
+          }
         />
       }
     >
+      {isCalendarOpen && (
+        <>
+          <div className="fixed inset-0 bg-black/30 bg-opacity-50 z-10" onClick={() => setIsCalendarOpen(false)} />
+          <div
+            className={`absolute left-1/2 -translate-x-1/2 w-[280px] mt-1 bg-white/10 backdrop-blur-[20px] rounded-lg border-2 border-primary-50 shadow-lg z-20 overflow-hidden`}
+          >
+            <InputCalendar selectedDate={selectedDate} onSelectDate={handleDateSelected} />
+          </div>
+        </>
+      )}
       <div className="flex flex-col gap-8">
         <span className="w-10" aria-hidden="true" role="presentation"></span>
-        {/* <header className="flex justify-between items-center">
-          <button
-            className="flex w-10 h-10 justify-center items-center rounded-xl backdrop-blur-[5px] shadow-[2px_2px_4px_0px_rgba(0,0,0,0.25),-2px_-2px_4px_0px_hsla(0, 0%, 100%, 0.1)]"
-            onClick={() => {
-              router.back();
-            }}
-          >
-            <LeftIcon />
-          </button>
-          <div className="flex gap-1">
-            <h1 className="text-center text-sm font-medium">
-              {selectedDate.getFullYear()}년 {selectedDate.getMonth() + 1}월
-            </h1>
-            <button
-              onClick={() => {
-                // <Month selectedDate={selectedDate} changeDate={changeDate} />
-              }}
-            >
-              <DownIcon />
-            </button>
-          </div>
-          <button
-            className="flex w-10 h-10 justify-center items-center rounded-xl backdrop-blur-[5px] shadow-[2px_2px_4px_0px_rgba(0,0,0,0.25),-2px_-2px_4px_0px_hsla(0, 0%, 100%, 0.1)]"
-            onClick={handleAddButtonClick}
-          >
-            <PlusIcon className="w-6 h-6" />
-          </button>
-        </header> */}
         <Week />
         <DietList />
       </div>
