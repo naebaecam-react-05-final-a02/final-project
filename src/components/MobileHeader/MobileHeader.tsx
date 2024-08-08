ba7dc0cb7c1b3e509fe3e3ec0f0a9a8e41702b8c
kwakjaehun <jh5414092@icloud.com>
2024-08-08 19:54:04 +0900

    Fix : 기능 없는 버튼 제거

--- src/components/MobileHeader/MobileHeader.tsx
+++ src/components/MobileHeader/MobileHeader.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import NotificationButton from '../ButtonIcon/NotificationButton';
-import SearchButton from '../ButtonIcon/SearchButton';
 import UserProfile from '../UserProfile/UserProfile';
 
 const DefaultHeader = () => {
@@ -10,7 +9,6 @@ const DefaultHeader = () => {
       {/* 다른 사용자 정보 표시 */}
       <UserProfile />
       <div className="flex items-center gap-4">
-        <SearchButton />
         <NotificationButton />
       </div>
     </div>
