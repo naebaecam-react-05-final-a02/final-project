6367262cdb8cbf17850d740e8da012d8f5e81c31
butterbeetle <butterbeetle@naver.com>
2024-08-11 16:53:05 +0900

    Update: 하루전에 알림테이블에 넣는 함수

--- supabase/functions/sendChallengeReminder/index.ts
+++ supabase/functions/sendChallengeReminder/index.ts
@@ -11,29 +11,33 @@ Deno.serve(async () => {
     const supabase = createClient(supabaseUrl, supabaseKey);
 
     const today = new Date(Date.now() + 9 * 60 * 60 * 1000);
-    const todayStr = today.toISOString().split('T')[0];
-    console.log('Today___', todayStr);
-    // 다 가져와도 되나?
-    const { data: challenges, error } = await supabase.from('challenges').select('id,startDate,endDate,isProgress');
+    today.setDate(today.getDate() + 1);
+    const yesterdayStr = today.toISOString().split('T')[0];
+    console.log('Yesterday___', yesterdayStr);
+
+    const { data: challenges, error } = await supabase
+      .from('challenges')
+      .select('*,participants:challengeParticipants(*)')
+      .eq('startDate', yesterdayStr);
 
     if (error) {
       console.log(error);
       return new Response(JSON.stringify({ error }), { status: 500, headers: { 'Content-Type': 'application/json' } });
     }
 
-    const promises = challenges.map((challenge: any) => {
-      let isProgress = challenge.isProgress;
-      if (challenge.endDate < todayStr) {
-        // 시작일 상관없고
-        // 종료일이 오늘보다 작다면 끝난 챌린지
-        isProgress = false;
-      } else if (challenge.startDate <= todayStr && challenge.endDate >= todayStr) {
-        // 시작일이 오늘과 같거나 이전이고
-        // 종료일이 오늘과 같거나 더 크면 진행 중인 챌린지
-        isProgress = true;
-      }
-
-      return supabase.from('challenges').update({ isProgress }).eq('id', challenge.id).select('id, isProgress');
+    const promises = challenges.flatMap((challenge: any) => {
+      return challenge.participants.map((participant: any) => {
+        return supabase
+          .from('alarm')
+          .insert({
+            createdAt: new Date(),
+            idForURL: challenge.id,
+            targetUserId: participant.userId,
+            isRead: false,
+            type: 'challenge',
+          })
+          .select('id');
+      });
     });
 
     const results = await Promise.all(promises);
