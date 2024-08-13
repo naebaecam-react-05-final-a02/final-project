f8c4a93e9a3eb261f6d9bd1cf0f3e4ef20586319
2hanbyeol1 <2hanbyeol1@naver.com>
2024-08-13 17:57:15 +0900

    style: 식단 조회에서 kcal 분리, Chip 아이콘 크기 24px

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -11,12 +11,12 @@ interface ChipProps {
 const Chip = ({ food, isActive, handleDelete, onClick }: ChipProps & ComponentProps<'button'>) => {
   return (
     <button
-      className={`flex gap-3 items-center flex-none p-1 pr-3 bg-white bg-opacity-10 rounded-full border ${
-        isActive ? 'border-[#12F287]' : 'border-transparent'
+      className={`flex gap-2 items-center flex-none p-1 pr-3 bg-white bg-opacity-10 rounded-full border ${
+        isActive ? 'border-primary-100' : 'border-transparent'
       }`}
       onClick={onClick}
     >
-      <div className="p-2 text-lg">{food.foodType}</div>
+      <div className="p-2 text-2xl leading-none">{food.foodType}</div>
       <div className="flex flex-col justify-center items-start">
         <span className="text-sm whitespace-nowrap leading-tight pr-1">{food.foodName || '-'}</span>
         <span className="text-[10px] opacity-30 whitespace-nowrap leading-tight">{food.kcal || 0} Kcal</span>
