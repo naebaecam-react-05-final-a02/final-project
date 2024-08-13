a841d4403f36be01d508939811bda23b364f7710
sorakim <sora6418@gmail.com>
2024-08-13 23:05:48 +0900

    update: 투표기능을 위한 라우트 로직 변경

--- src/app/api/community/vote/route.ts
+++ src/app/api/community/vote/route.ts
@@ -1,3 +1,4 @@
+import { usersQueryKeys } from '@/hooks/auth/queries';
 import { createClient } from '@/supabase/server';
 import { NextRequest, NextResponse } from 'next/server';
 
@@ -5,7 +6,7 @@ export async function POST(request: NextRequest) {
   const supabase = createClient();
 
   try {
-    const { title, items } = await request.json();
+    const { title, items, id } = await request.json();
 
     // 사용자 인증 가져오기
     const {
@@ -17,30 +18,52 @@ export async function POST(request: NextRequest) {
     }
 
     const userId = user.id;
+    console.log('@@title', title);
 
-    // 필수 필드 검증
-    if (!title || !Array.isArray(items) || items.length < 2) {
-      return NextResponse.json({ message: '제목과 최소 두 개의 항목을 입력해야 합니다.' }, { status: 400 });
+    if (!id) {
+      // 필수 필드 검증
+      if (!title || !Array.isArray(items) || items.length < 2) {
+        console.log('@@1', 1);
+        return NextResponse.json({ message: '제목과 최소 두 개의 항목을 입력해야 합니다.' }, { status: 400 });
+      }
+      const { data, error } = await supabase
+        .from('communityVotes')
+        .insert([
+          {
+            title,
+            items: JSON.stringify(items), // items 배열을 JSON 문자열로 변환하여 저장
+          },
+        ])
+        .select()
+        .single();
+      if (error) {
+        throw error;
+      }
+    } else {
+      // 데이터베이스에 투표 데이터 추가
+      const { data, error: voteError } = await supabase
+        .from('communityVotes')
+        .update([
+          {
+            items: JSON.stringify(items), // items 배열을 JSON 문자열로 변환하여 저장
+          },
+        ])
+        .eq('id', id)
+        .select()
+        .single();
+      const { error: voterError } = await supabase
+        .from('communityVoter')
+        .upsert([
+          {
+            id,
+            userId,
+          },
+        ])
+        .select()
+        .single();
     }
 
-    // 데이터베이스에 투표 데이터 추가
-    const { data, error } = await supabase
-      .from('communityVotes')
-      .insert([
-        {
-          title,
-          items: JSON.stringify(items), // items 배열을 JSON 문자열로 변환하여 저장
-          userId,
-        },
-      ])
-      .select()
-      .single();
-
-    if (error) {
-      throw error;
-    }
-
-    return NextResponse.json({ message: '투표가 성공적으로 등록되었습니다.', data }, { status: 200 });
+    return NextResponse.json({ message: '투표가 성공적으로 등록되었습니다.' }, { status: 200 });
   } catch (error) {
     console.error('@@ error', error);
     return NextResponse.json(
