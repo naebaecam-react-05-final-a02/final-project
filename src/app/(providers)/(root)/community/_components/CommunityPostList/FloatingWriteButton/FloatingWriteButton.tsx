f479a434d1a0e0623b79d843b000f84c327bfd0c
Jongha <jongha1230@gmail.com>
2024-08-14 10:59:43 +0900

    fix: 플로팅 버튼 리스트 기준으로 고정되게 수정

--- src/app/(providers)/(root)/community/_components/CommunityPostList/FloatingWriteButton/FloatingWriteButton.tsx
+++ src/app/(providers)/(root)/community/_components/CommunityPostList/FloatingWriteButton/FloatingWriteButton.tsx
@@ -1,43 +1,33 @@
 'use client';
 
 import Puls from '@/icons/Puls';
-import Link from 'next/link';
-import { useState } from 'react';
 
 interface FloatingWriteButtonProps {
   inView: boolean;
 }
 
 const FloatingWriteButton = ({ inView }: FloatingWriteButtonProps) => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const isExpanded = !inView || isHovered;
-
   return (
-    <Link href="/community/write">
-      <button
+    <button
+      className={`
+        transition-all duration-300 ease-in-out px-4 py-4
+        flex items-center justify-center z-50
+        h-14 bg-black text-white rounded-full border-2 border-whiteT-10
+        shadow-[-4px_-4px_8px_0px_rgba(18,242,135,0.10),_4px_4px_8px_0px_rgba(0,0,0,0.40)]
+        ${inView ? 'w-14 hover:w-[110px]' : 'w-[110px]'}
+      `}
+    >
+      <Puls className="flex-shrink-0 w-6 h-6 text-primary-100" />
+      <span
         className={`
-          transition-all duration-300 ease-in-out px-4 py-4
-          flex items-center justify-center pointer-events-auto
-          h-14 bg-black text-white rounded-full border-2 border-whiteT-10
-          shadow-[-4px_-4px_8px_0px_rgba(18,242,135,0.10),_4px_4px_8px_0px_rgba(0,0,0,0.40)]
-          ${isExpanded ? 'w-[110px]' : 'w-14'}
+          ml-1 text-[16px] font-semibold whitespace-nowrap overflow-hidden
+          transition-all duration-300
+          ${inView ? 'w-0 opacity-0 group-hover:w-auto group-hover:opacity-100' : 'w-auto opacity-100'}
         `}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
       >
-        <Puls className="flex-shrink-0 w-6 h-6 text-primary-100" />
-        <span
-          className={`
-            ml-1 text-[16px] font-semibold whitespace-nowrap overflow-hidden
-            transition-all duration-300
-            ${isExpanded ? 'w-auto opacity-100' : 'w-0 opacity-0'}
-          `}
-        >
-          글쓰기
-        </span>
-      </button>
-    </Link>
+        글쓰기
+      </span>
+    </button>
   );
 };
 
